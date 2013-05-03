2523cd2663c69069c562d200c40f2c1f918ab504
Rich Wallett <rich.wallett@gmail.com>
2013-05-03 14:34:41 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  
 end
